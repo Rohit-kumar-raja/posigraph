6772e2fc0a1b4e18ddf144b39b38fc189ba85abf
Raja <raja@raja.com>
2022-10-28 12:04:36 +0530

    all done

--- posi.php
+++ posi.php
@@ -1,25 +1,25 @@
 <?php
-if(basename($_SERVER['PHP_SELF']) == 'dashboard.php'){?>
+if (basename($_SERVER['PHP_SELF']) == 'dashboard.php') { ?>
     <div class="tabs-area">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 col-xs-12 tabs-body">
                     <div class="tabs">
-                        
+
                         <div class="tab-2 tab-1">
-                        <a class="first-tab" href="https://posigraph.com/posigraph_new/dashboard.php"> Feed </a>
-                        <input id="tab2-2" name="tabs-two" type="radio" checked="checked">
-                          
+                            <a class="first-tab" href="dashboard.php"> Feed </a>
+                            <input id="tab2-2" name="tabs-two" type="radio" checked="checked">
+
                         </div>
 
                         <div class="tab-2">
-                        <a class="first-tab" href="https://posigraph.com/posigraph_new/battle/battle.php"> Story </a>                            
-                            <input id="tab2-1" name="tabs-two" type="radio">                           
+                            <a class="first-tab" href="battle/battle.php"> Battle </a>
+                            <input id="tab2-1" name="tabs-two" type="radio">
                         </div>
 
                         <div class="tab-2">
-                        <a href="#" class="first-tab"> Board </a>
-                            <input id="tab2-3" name="tabs-two" type="radio">                           
+                            <a href="#" class="first-tab"> Board </a>
+                            <input id="tab2-3" name="tabs-two" type="radio">
                         </div>
 
                     </div>
@@ -29,73 +29,64 @@ if(basename($_SERVER['PHP_SELF']) == 'dashboard.php'){?>
     </div>
     <!-- end tabs -->
 <?php
-}elseif(basename($_SERVER['PHP_SELF']) == 'home.php'){?>
+} elseif (basename($_SERVER['PHP_SELF']) == 'home.php') { ?>
     <div class="tabs-area">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 col-xs-12 tabs-body">
-                <div class="tabs">
-                    
-                    <div class="tab-2 tab-1">
-                    <a class="first-tab" href="https://posigraph.com/posigraph_new/dashboard.php"> Feed </a>
-                    <input id="tab2-2" name="tabs-two" type="radio">
-                      
-                    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-xs-12 tabs-body">
+                    <div class="tabs">
 
-                    <div class="tab-2">
-                    <a class="first-tab" href="https://posigraph.com/posigraph_new/battle/battle.php"> Story </a>                            
-                        <input id="tab2-1" name="tabs-two" type="radio" checked="checked">                           
-                    </div>
+                        <div class="tab-2 tab-1">
+                            <a class="first-tab" href="dashboard.php"> Feed </a>
+                            <input id="tab2-2" name="tabs-two" type="radio">
 
-                    <div class="tab-2">
-                    <a href="#" class="first-tab"> Board </a>
-                        <input id="tab2-3" name="tabs-two" type="radio">                           
-                    </div>
+                        </div>
 
+                        <div class="tab-2">
+                            <a class="first-tab" href="battle/battle.php"> Battle </a>
+                            <input id="tab2-1" name="tabs-two" type="radio" checked="checked">
+                        </div>
+
+                        <div class="tab-2">
+                            <a href="#" class="first-tab"> Board </a>
+                            <input id="tab2-3" name="tabs-two" type="radio">
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- end tabs -->
+    <!-- end tabs -->
 
-<?php }else{?>
+<?php } else { ?>
     <div class="tabs-area">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 col-xs-12 tabs-body">
-                <div class="tabs">
-                    
-                    <div class="tab-2 tab-1">
-                    <a class="first-tab" href="https://posigraph.com/posigraph_new/dashboard.php"> Feed </a>
-                    <input id="tab2-2" name="tabs-two" type="radio">
-                      
-                    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-xs-12 tabs-body">
+                    <div class="tabs">
 
-                    <div class="tab-2">
-                    <a class="first-tab" href="https://posigraph.com/posigraph_new/battle/battle.php"> Story </a>                            
-                        <input id="tab2-1" name="tabs-two" type="radio">                           
-                    </div>
+                        <div class="tab-2 tab-1">
+                            <a class="first-tab" href="dashboard.php"> Feed </a>
+                            <input id="tab2-2" name="tabs-two" type="radio">
 
-                    <div class="tab-2">
-                    <a href="#" class="first-tab"> Board </a>
-                        <input id="tab2-3" name="tabs-two" type="radio" checked="checked">                           
-                    </div>
+                        </div>
 
+                        <div class="tab-2">
+                            <a class="first-tab" href="battle/battle.php"> Battle </a>
+                            <input id="tab2-1" name="tabs-two" type="radio">
+                        </div>
+
+                        <div class="tab-2">
+                            <a href="#" class="first-tab"> Board </a>
+                            <input id="tab2-3" name="tabs-two" type="radio" checked="checked">
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 <?php }
 ?>
-
-    
-
-
-
-
-
-   
-   
